8a3de6cc2f47d5fe3c80138b742e46b16622e54c
BRIJRAJSINH JADEJA <147186899+brijrajsinhjadeja@users.noreply.github.com>
2025-02-20 08:55:02 +0530

    final output

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,10 @@ const Footer = () => {
       <div className="footer-content">
         <p>&copy; {new Date().getFullYear()} Taskify. All rights reserved.</p>
         <p>
-          Contact us: <a href="mailto:support@taskify.com">support@taskify.com</a>
+          Contact us: <a href="mailto:support@taskify.com">support@taskify.com</a><br/>
         </p>
+        <h3>          Developed By : Brijrajsinh Jadeja <a href="https://3waytech.co/" >@ 3way Technology</a>
+        </h3>
       </div>
     </footer>
   );
