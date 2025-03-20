567f6e027b83ede30d577c8bdc3251cece17920d
BRIJRAJSINH JADEJA <147186899+brijrajsinhjadeja@users.noreply.github.com>
2025-03-20 23:46:20 +0530

    add contect btn

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,9 @@ import { GrTask } from "react-icons/gr";
 import { MdDashboard, MdLogin, MdOutlineTaskAlt, MdAddTask, MdPendingActions, MdCloudDone, MdOutlineAccessTimeFilled, MdQueryStats } from "react-icons/md";
 import { GrInProgress } from "react-icons/gr";
 import { Link } from "react-router-dom";
-
+import React from 'react';
+import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 
 const Sidebar = () => {
@@ -89,7 +91,29 @@ const Sidebar = () => {
                         </span>
                     </Link>
                 </ul>
-                
+                <div className="mt-auto p-4">
+      {/* Contact Button */}
+      <button 
+        onClick={() => window.location.href = 'mailto:support@taskify.com'} 
+        className="w-full bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md mb-4 flex items-center justify-center"
+      >
+        <MdEmail className="mr-2" />
+        Contact Us
+      </button>
+
+      {/* Social Media Links */}
+      <div className="flex justify-center space-x-4">
+        <a href="https://www.facebook.com/brijarajsinh.jadeja.908" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-blue-500">
+          <FaFacebook size={20} />
+        </a>
+        <a href="https://www.linkedin.com/in/brijrajsinh-jadeja-08026233a/" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-blue-600">
+          <FaLinkedin size={20} />
+        </a>
+        <a href="https://github.com/brijrajsinhjadeja" target="_blank" rel="noopener noreferrer" className="text-gray-400 hover:text-gray-800">
+          <FaGithub size={20} />
+        </a>
+      </div>
+    </div>
             </nav>
         </div>
     );
